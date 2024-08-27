1b26d6ce6fcce51f472cf42a22fa903aefa6f3d7
yuzhongchun17 <zhongchun.yu@outlook.com>
2024-08-27 01:27:45 -0700

    udpate cameraparameters

--- grasp_detection/utils/camera.py
+++ grasp_detection/utils/camera.py
@@ -2,16 +2,16 @@ from dataclasses import dataclass
 from typing import Type
 
 import numpy as np
-# from PIL import Image
+from PIL import Image
 
 @dataclass
 class CameraParameters:
+    width: int
+    height: int
     fx: float
     fy: float
     cx: float
     cy: float
     colors: np.ndarray
     depths: np.ndarray
-    
-    # head_tilt: float
-    # image: Type[Image.Image]
+    masks: np.ndarray
